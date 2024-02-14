7c62bb236bef75d6f52fa7267a82254fcd00815d
mexonix <16181821+mexonix@users.noreply.github.com>
2024-02-15 00:45:54 +0100

    added detection of scroll direction

---
+++ src/utils/hooks/useScrollDirection.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export const useScrollDirection = () => {
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down' | null>(
+    null,
+  );
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.scrollY;
+      const direction = scrollY > lastScrollY ? 'down' : 'up';
+      if (
+        direction !== scrollDirection &&
+        (scrollY - lastScrollY > 1 || scrollY - lastScrollY < -1)
+      ) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener('scroll', updateScrollDirection); // add event listener
+    return () => {
+      window.removeEventListener('scroll', updateScrollDirection); // clean up
+    };
+  }, [scrollDirection]);
+
+  return scrollDirection;
+};
