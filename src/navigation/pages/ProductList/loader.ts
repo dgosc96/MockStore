61b5e2a68a4219ef27aa24df538d68ab6415a6ae
mexonix <16181821+mexonix@users.noreply.github.com>
2024-02-15 00:53:03 +0100

    minor changes

--- src/navigation/pages/ProductList/loader.ts
+++ src/navigation/pages/ProductList/loader.ts
@@ -1,13 +1,14 @@
 import type { QueryClient } from '@tanstack/react-query';
-import { productQueryAll } from '../../../api/products';
+import { productQueryAll } from '../../../adapters/products';
 import { preloadImage } from '../../../utils/preloadImage';
 
-export const productListLoader = (queryClient: QueryClient) => async () => {
+export const productListLoader = async (queryClient: QueryClient) => {
   const imagePromises: Array<Promise<any>> = [];
   const products = await queryClient.fetchQuery(productQueryAll());
 
   products.forEach(({ image }) => imagePromises.push(preloadImage(image)));
 
   await Promise.all(imagePromises);
+
   return products;
 };
