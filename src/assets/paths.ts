e8b7b53170de185769ee5c258dc47472c12e3ebc
mexonix <16181821+mexonix@users.noreply.github.com>
2024-02-15 00:25:26 +0100

    added file for asset paths

---
+++ src/assets/paths.ts
@@ -0,0 +1,5 @@
+import heroImagePath from '/img/hero-image.jpg';
+import appLogoImagePath from '/svg/MockStore-logo.svg';
+import placeholderImagePath from '/svg/image-placeholder.svg';
+
+export { heroImagePath, appLogoImagePath, placeholderImagePath };
