76b471c04471423b58156e10fe81a3a86bbe4460
mexonix <16181821+mexonix@users.noreply.github.com>
2024-02-15 00:22:22 +0100

    improved script for generating file of tailwind screens obj

--- scripts/genfile-tailwind-obj.js
+++ scripts/genfile-tailwind-obj.js
@@ -1,12 +1,18 @@
 import resolveConfig from 'tailwindcss/resolveConfig.js';
 import TWConfigFile from '../tailwind.config.js';
 import fs from 'fs';
+import path from 'path';
 
 const fullConfig = resolveConfig(TWConfigFile);
 
 const screensObj = fullConfig.theme.screens;
 
-fs.writeFileSync(
-  `${process.cwd()}/src/lib/generated/tailwind-config-objs.ts`,
-  `export const screens = ${JSON.stringify(screensObj)};`,
-);
+// Define the directory and file
+const dir = process.cwd() + '/generated';
+const file = path.join(dir, 'tailwind-config-objs.ts');
+
+// Create the directory if it does not exist
+fs.mkdirSync(dir, { recursive: true });
+
+// Create or overwrite the file
+fs.writeFileSync(file, `export const screens = ${JSON.stringify(screensObj)};`);
